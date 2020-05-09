076b57636f395473ee67e93ccd1ccf6ab67b2e11
alessandroprudencio <alessandroconectado@gmail.com>
2020-05-09 13:58:21 -0400

    mudando retorno dos endpoints

--- src/resolvers.js
+++ src/resolvers.js
@@ -10,21 +10,17 @@ export const resolvers = {
         }
     },
     Mutation: {
-        async createUser(_, { input }) {
+        async createUser(_, input) {
             const { name, email, password } = input
             const resultado = await knex('users').insert({ name, email, password })
             return await knex('users').where({ id: resultado[0] }).first()
         },
-        async updateUser(_, { id, input }) {
-
-            const { name, email, password } = input
-
+        async updateUser(_, { id, name, email, password }) {
             await knex('users').update({ name, email, password }).where({ id })
             return knex('users').where({ id }).first()
-
         },
-        async deleteUser(_, { id }) {
-            return await knex('users').where('id',id).delete()
+        async deleteUser(_, id) {
+            return await knex('users').where('id', id).delete()
         }
     }
 }
